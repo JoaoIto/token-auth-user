6fe63127254824cd8e243e4fd22eea8f1c6ae944
JoaoIto <joaovictorpfr@gmail.com>
2024-02-20 16:49:52 -0300

    add token auth to user, get users, and token add for user exist int /login post

---
+++ src/server.ts
@@ -0,0 +1,78 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+const app = express();
+const port = 3000;
+const secretKey = 'secretKey';
+
+// Conectar ao banco de dados MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/mydatabase');
+
+// Verificar se a conexão foi bem-sucedida
+mongoose.connection.on('connected', () => {
+    console.log('Conectado ao banco de dados!');
+    // Você também pode exibir algumas informações sobre o banco de dados conectado
+    console.log('Informações do banco de dados:');
+    console.log(`Nome do banco de dados: ${mongoose.connection.name}`);
+    console.log(`Nome das tabelas banco de dados: ${mongoose.connection.db.collections()}`);
+    console.log(`Host: ${mongoose.connection.host}`);
+    console.log(`Porta: ${mongoose.connection.port}`);
+});
+
+// Verificar se a conexão falhou
+mongoose.connection.on('error', (error) => {
+    console.error('Erro ao conectar ao banco de dados:', error);
+});
+
+// Definir o schema do usuário
+const userSchema = new mongoose.Schema({
+    cpf: String,
+    senha: String,
+});
+
+// Definir o modelo de usuário
+const UserModel = mongoose.model('User', userSchema);
+
+app.use(bodyParser.json());
+
+app.post('/login', async (req, res) => {
+    const { cpf, senha } = req.body;
+    try {
+        // Procurar usuário no banco de dados
+        const user = await UserModel.findOne({ cpf });
+        if (!user) {
+            return res.status(401).json({ error: 'Usuário não encontrado' });
+        }
+        // Verificar a senha
+        if (user.senha !== senha) {
+            return res.status(401).json({ error: 'Senha incorreta' });
+        }
+        // Gerar token JWT
+        const token = jwt.sign({ cpf }, secretKey);
+        res.json({ token });
+    } catch (error) {
+        console.error('Erro durante a autenticação:', error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+app.get('/users', async (req, res) => {
+    try {
+        console.log('\nBuscando usuários...\n');
+        // Buscar todos os usuários no banco de dados
+        const users = await UserModel.find();
+        console.log('Usuários encontrados:', users);
+        // Retornar a lista de usuários
+        res.json({ users });
+    } catch (error) {
+        console.error('Erro ao buscar usuários:', error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
