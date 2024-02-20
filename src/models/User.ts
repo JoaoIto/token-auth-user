6fd953507e00225d1104dfae723193217c986c15
JoaoIto <joaovictorpfr@gmail.com>
2024-02-20 17:17:49 -0300

    userModel userSchema mongoose

---
+++ src/models/User.ts
@@ -0,0 +1,10 @@
+// Definir o schema do usuário
+import mongoose from "mongoose";
+
+export const userSchema = new mongoose.Schema({
+    cpf: String,
+    senha: String,
+});
+
+// Definir o modelo de usuário
+export const UserModel = mongoose.model('User', userSchema);
