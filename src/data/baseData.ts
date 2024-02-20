9917e8e59c57d75a8252d36365a69914d4fe0969
JoaoIto <joaovictorpfr@gmail.com>
2024-02-20 16:49:13 -0300

    add datafake in database

---
+++ src/data/baseData.ts
@@ -0,0 +1,35 @@
+import fs from 'fs';
+
+// Função para gerar CPF aleatório
+function generateRandomCPF() {
+    let cpf = '';
+    for (let i = 0; i < 11; i++) {
+        cpf += Math.floor(Math.random() * 10).toString();
+    }
+    return cpf;
+}
+
+// Função para gerar senha aleatória
+function generateRandomPassword(length: number) {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return password;
+}
+
+// Gerar dados falsos
+const numberOfUsers = 10;
+const fakeData = [];
+
+for (let i = 0; i < numberOfUsers; i++) {
+    const cpf = generateRandomCPF();
+    const senha = generateRandomPassword(4);
+    fakeData.push({ cpf, senha });
+}
+
+// Escrever os dados falsos em um arquivo JSON
+fs.writeFileSync('fakeData.json', JSON.stringify(fakeData, null, 2));
+
+console.log('Dados falsos gerados e gravados no arquivo fakeData.json');
